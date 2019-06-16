37a9bf09fb986eb6d1d2e3f8a584662c5e61c645
dkozak <dkozak94@gmail.com>
2019-06-16 22:37:05 +0100

    init commit

---
+++ home-server/src/test/java/io/dkozak/home/control/server/server/ServerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.dkozak.home.control.server.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
