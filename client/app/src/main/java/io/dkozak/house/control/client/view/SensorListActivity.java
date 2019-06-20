5c6a83bcb3f007e08a15767cc6f5c9e1181755c1
dkozak <dkozak94@gmail.com>
2019-06-20 22:26:41 +0100

    logout support

--- client/app/src/main/java/io/dkozak/house/control/client/view/SensorListActivity.java
+++ client/app/src/main/java/io/dkozak/house/control/client/view/SensorListActivity.java
@@ -2,12 +2,20 @@ package io.dkozak.house.control.client.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
@@ -65,5 +73,31 @@ public class SensorListActivity extends SensorAwareActivity {
     @Override
     protected void onNewUserSensors(List<Sensor> userSensors) {
         adapter.update(userSensors);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.rule_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(SensorListActivity.this, "Logout successful", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(SensorListActivity.this, LoginActivity.class));
+                            }
+                        });
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
