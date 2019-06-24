2d5d4976e0f5b5c52c47dc6205539b597075065c
dkozak <dkozak94@gmail.com>
2019-06-24 14:02:05 +0100

    update zip file name

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ZIP_NAME="dkozak.zip"
+ZIP_NAME="pedwm_18180044.zip"
 rm -f ${ZIP_NAME}
 zip -r ${ZIP_NAME} client gateway imgs sensors server build.gradle log.properties README.md run.sh settings.gradle DOC.pdf -x "*/.git/**" -x "*/build/**" -x "*/.idea/**" -x "*/out/**"
